2cb254654a0768626f9a326404324cc1dd453659
nguyentoann21 <nguyenvantoan28082001@gmail.com>
2023-05-10 19:36:07 +0700

    done UI notfound404

--- mf-frontend/src/pages/NotFound404/index.js
+++ mf-frontend/src/pages/NotFound404/index.js
@@ -1,8 +1,17 @@
+import './error404.scss';
+
 function NotFound404() {
     return (
         <div className='error-not-found-404'>
-            <h1>404</h1>
-            <h2>Oops, This page not found!</h2>
+            <div className='error-not-found-container'>
+                <div className='gif-video-404'>
+                    <img src="/assets/images/dino.gif" alt="dino-error"/>
+                </div>
+                <div className='text-404'>
+                    <h1>404</h1>
+                    <h2>Oops, This page not found!</h2>
+                </div>
+            </div>
         </div>
     )
 }
--- mf-frontend/src/pages/Ranking/index.js
+++ mf-frontend/src/pages/Ranking/index.js
@@ -1,9 +1,93 @@
-function Ranking() {
-    return(
-        <div>
-            Ranking
-        </div>
-    );
-}
+import React, { useState } from 'react';
+import { FaMedal } from 'react-icons/fa';
+import './rank.css';
+
+const leaderboardData = {
+  levels: [
+    { no: 1, name: 'John', level: 'Level 10' },
+    { no: 2, name: 'Alice', level: 'Level 8' },
+    { no: 3, name: 'Bob', level: 'Level 7' },
+    { no: 4, name: 'Sarah', level: 'Level 5' },
+    { no: 5, name: 'Peter', level: 'Level 2' },
+  ],
+  rankings: [
+    { no: 1, name: 'John', rank: 'Expert' },
+    { no: 2, name: 'Alice', rank: 'Pro' },
+    { no: 3, name: 'Bob', rank: 'Advanced' },
+    { no: 4, name: 'Sarah', rank: 'Intermediate' },
+    { no: 5, name: 'Peter', rank: 'Beginner' },
+  ],
+  challenges: [
+    { no: 1, name: 'Challenge 1', challenge: 'Easy' },
+    { no: 2, name: 'Challenge 2', challenge: 'Medium' },
+    { no: 3, name: 'Challenge 3', challenge: 'Hard' },
+    { no: 4, name: 'Challenge 4', challenge: 'Defeat' },
+  ],
+};
+
+const Ranking = () => {
+  const [activeTab, setActiveTab] = useState('levels');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="leaderboard-container">
+      <div className="tabs-container">
+        <button
+          className={`tab ${activeTab === 'levels' ? 'active' : ''}`}
+          onClick={() => handleTabChange('levels')}
+        >
+          Level
+        </button>
+        <button
+          className={`tab ${activeTab === 'rankings' ? 'active' : ''}`}
+          onClick={() => handleTabChange('rankings')}
+        >
+          Rank
+        </button>
+        <button
+          className={`tab ${activeTab === 'challenges' ? 'active' : ''}`}
+          onClick={() => handleTabChange('challenges')}
+        >
+          Challenge
+        </button>
+      </div>
+
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>{activeTab === 'levels' ? 'Level' : activeTab === 'rankings' ? 'Rank' : 'Challenge'}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leaderboardData[activeTab].map((item) => (
+              <tr key={item.no}>
+                <td>
+                  {item.no === 1 ? (
+                    <FaMedal className='gold'/>
+                  ) : item.no === 2 ? (
+                    <FaMedal className='silver'/>
+                  ) : item.no === 3 ? (
+                    <FaMedal className='bronze'/>
+                  ) : (
+                    <div className="rank">{item.no}</div>
+                  )}
+                </td>
+                <td>{item.name}</td>
+                <td>{item[activeTab === 'levels' ? 'level' : activeTab === 'rankings' ? 'rank' : 'challenge']}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+  );
+};
 
 export default Ranking;
+
