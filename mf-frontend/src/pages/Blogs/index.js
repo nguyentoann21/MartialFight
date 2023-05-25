d33c3ffe5f526a20d1a80715bfa6765200dba521
nguyentoann21 <nguyenvantoan28082001@gmail.com>
2023-05-25 19:37:19 +0700

    done about blogs

--- mf-frontend/src/pages/Blogs/index.js
+++ mf-frontend/src/pages/Blogs/index.js
@@ -9,31 +9,36 @@ const Blogs = () => {
       id: 1, 
       title: 'Update version 1.1', 
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec 123.',
-      date: '2023/05/01'
+      date: '2023/05/01',
+      img: 'https://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg'
     },
     { 
       id: 2,
       title: 'Update version 1.2', 
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec.',
-      date: '2023/06/01'
+      date: '2023/06/01',
+      img: 'https://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg'
     },
     { 
       id: 3,
       title: 'Update version 1.3', 
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec.',
-      date: '2023/06/15'
+      date: '2023/06/15',
+      img: 'https://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg'
     },
     { 
       id: 4,
       title: 'Update version 1.4', 
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec.',
-      date: '2023/07/01'
+      date: '2023/07/01',
+      img: 'https://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg'
     },
     { 
       id: 5,
       title: 'Update version 1.5', 
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi tempor efficitur quam, et hendrerit neque vestibulum nec.',
-      date: '2023/07/15'
+      date: '2023/07/15',
+      img: ''
     },
   ];
 
@@ -104,6 +109,12 @@ const Blogs = () => {
                     <div className='dialog-content-main'>
                       <h2>{selectedBlog.title}</h2>
                       <h4>{selectedBlog.date}</h4>
+                      <p>{selectedBlog.content}</p>
+                      {selectedBlog.img && 
+                        <img src={selectedBlog.img} alt='Blog' />
+                      }
+                      <p>{selectedBlog.content}</p>
+                      <p>{selectedBlog.content}</p>
                       <p>{selectedBlog.content}</p>
                     </div>
                     <div className='dialog-content-button'>
